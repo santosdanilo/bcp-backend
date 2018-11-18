d9e8feb617ccc58407f69f0bbe982816b4de2bbe
Danilo Santos <dandesenvolvimento@gmail.com>
2018-11-18 12:47:28 -0200

    Basic file

---
+++ url.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import json
+from flask import Response
+
+from digger.databases import core
+from digger.databases import cedet
+from digger.databases import loja_integrada
+
+app = Flask(__name__,
+static_folder="./dist/static",
+template_folder="./dist")
+
+@app.route('/') 
+def hello_world(): 
+    return 'Hello, World!'
+
+@app.route('/api/<bookStore>/book/<bookName>', methods=['GET']) 
+def getStoreBooks(bookStore,bookName):
+    getBook = core.getBookStore(bookStore) 
+    js = json.dumps(getBook(bookName))
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
